22abf31459cc2cbd6415388f41b382bec998b2a5
Paulo Marchi <marchipaulo@gmail.com>
2022-06-28 15:29:05 -0300

    Update department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -127,7 +127,7 @@ public class DepartmentFormController implements Initializable{
 		}
 		
 		txtId.setText(String.valueOf(entity.getId()));
-		txtId.setText(entity.getName());
+		txtName.setText(entity.getName());
 	}
 	
 	private void setErrorMessages(Map<String, String> errors) {
